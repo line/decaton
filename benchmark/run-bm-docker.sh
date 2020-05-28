eea9a4d8253c2feb343aee1dd92b7a0582fd7640
Yuto Kawamura <kawamuray.dadada@gmail.com>
2020-05-28 16:12:33 +0900

    Add Dockerfile to kick benchmark on linux platform from osx

---
+++ benchmark/run-bm-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+opts=""
+while [[ "$1" == '-'* ]]; do
+    opts="$opts $1"
+    shift
+done
+
+root_dir=$(cd $(dirname $0); pwd)
+exec docker run --rm \
+     --network host --cap-add NET_ADMIN --cap-add SYS_PTRACE  \
+     -e DEBM_SKIP_SETCAP=yes \
+     -v $root_dir:/decaton \
+     $opts \
+     decaton-benchmark:latest "$@"
