05409e71b1fc0d204eb899a34c4dffc739a43774
RyotaYamagishi <ryota.yamagishi@linecorp.com>
2021-12-09 11:03:50 +0900

    Fix BatchingProcessor

--- processor/src/main/java/com/linecorp/decaton/processor/processors/BatchingProcessor.java
+++ processor/src/main/java/com/linecorp/decaton/processor/processors/BatchingProcessor.java
@@ -17,221 +17,121 @@
 package com.linecorp.decaton.processor.processors;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.BiConsumer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.linecorp.decaton.processor.DecatonProcessor;
+import com.linecorp.decaton.processor.DeferredCompletion;
 import com.linecorp.decaton.processor.ProcessingContext;
+import com.linecorp.decaton.processor.Completion;
 
-import lombok.ToString;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
-public class BatchingProcessor<T> implements DecatonProcessor<T> {
-    private static final Logger logger = LoggerFactory.getLogger(CompactionProcessor.class);
+abstract public class BatchingProcessor<T> implements DecatonProcessor<T> {
 
-    BiConsumer<String, List<BufferedTask<T>>> processor;
-    long lingerMillis;
-    int capacity;
-    long maxRetryCount;
+    private final ScheduledExecutorService executor;
+    private final List<BatchingTask<T>> windowedTasks = Collections.synchronizedList(new ArrayList<>());
+    private final long lingerMillis;
+    private final int capacity;
 
-    private final ConcurrentMap<String, BufferedTaskGroup> windowedTasks = new ConcurrentHashMap<>();
-    final ScheduledExecutorService scheduler = initScheduler();
+    @Getter
+    @Accessors(fluent = true)
+    public static class BatchingTask<T> {
+        @Getter(AccessLevel.NONE)
+        public final Completion completion;
+        public final ProcessingContext<T> context;
+        public final T task;
 
-    protected ScheduledExecutorService initScheduler() {
-        return new ScheduledThreadPoolExecutor(1, r -> {
-            Thread th = new Thread(r);
-            th.setName("Decaton" + BatchingProcessor.class.getSimpleName()
-                       + "-Flusher/" + System.identityHashCode(this));
-            return th;
-        });
-    }
-
-    @ToString
-    static class BufferedTask<T> {
-        final T task;
-        final ProcessingContext<T> context;
-
-        BufferedTask(T task, ProcessingContext<T> context) {
-            this.task = task;
+        private BatchingTask(Completion completion, ProcessingContext<T> context, T task) {
+            this.completion = completion;
             this.context = context;
-            context.deferCompletion();
+            this.task = task;
         }
     }
 
-    class BufferedTaskGroup {
-        private final String key;
-        private final int capacity;
-        private final List<BufferedTask<T>> bufferedTasks = new ArrayList<>();
-        private final Future<?> scheduledFlush;
-        private final AtomicBoolean flushed = new AtomicBoolean();
-
-        // Run in Processor thread for this partition
-        BufferedTaskGroup(String key, int capacity) {
-            this.key = key;
-            this.capacity = capacity;
-            scheduledFlush = scheduleFlush();
-        }
-
-        // Run from Processor thread for this partition
-
-        /**
-         * Will reject new tasks is flush has already happened or is ongoing.
-         * @return whether the task was actually added.
-         */
-        boolean addBufferedTask(ProcessingContext<T> context, T task) {
-            if (!context.key().equals(key)) {
-                throw new RuntimeException("");
-            }
-            synchronized (bufferedTasks) {
-                if (isFull() || flushed.get()) {
-                    return false;
-                }
-                bufferedTasks.add(new BufferedTask<>(task, context));
-            }
-            return true;
-        }
-
-        private boolean isFull() {
-            return bufferedTasks.size() >= capacity;
-        }
-
-        private Future<?> scheduleFlush() {
-            Runnable flushTask = () -> {
-                try {
-                    flush();
-                } catch (InterruptedException e) {
-                    logger.error("interrupted while flushing task group {}", this, e);
-                    Thread.currentThread().interrupt();
-                }
-            };
-            return scheduler.schedule(flushTask, lingerMillis, TimeUnit.MILLISECONDS);
-        }
-
-        void flush() throws InterruptedException {
-            BufferedTaskGroup expected;
-            synchronized (bufferedTasks) {
-                if (flushed.getAndSet(true)) {
-                    logger.info("Already flushed: {}", this);
-                    return;
-                }
-                logger.debug("Flushing task group: {}", this);
-                expected = windowedTasks.remove(key);
-
-                if (!bufferedTasks.isEmpty()) {
-                    process();
-                }
-            }
-
-            // Prevent concurrent update from missing tasks.
-            // Since there is no synchronization on `windowedTasks`
-            // then during scheduled flush another task can be inserted instead
-            if (expected != null && expected != this && !expected.flushed.get()) {
-                synchronized (expected.bufferedTasks) {
-                    if (!expected.flushed.getAndSet(true) && !expected.bufferedTasks.isEmpty()) {
-                        logger.warn("Flushing other task group: {}", expected);
-                        expected.process();
-                    }
-                }
-            }
-        }
-
-        // Run in Processor thread for this partition
-        void flushNow() throws InterruptedException {
-            if (!scheduledFlush.isDone()) {
-                boolean cancelled = scheduledFlush.cancel(false);
-                logger.debug("{} previously scheduled flush for user {}", key,
-                             cancelled ? "Cancelled" : "Already cancelled");
-                flush();
-            }
-        }
-
-        private void process() throws InterruptedException {
-            try {
-                processor.accept(key, bufferedTasks);
-            } catch (RuntimeException e) {
-                // Only retryable tasks are retried.
-                for (BufferedTask<T> t : bufferedTasks) {
-                    handleTaskFailure(e, t.context, t.task);
-                }
-            } finally {
-                // complete successful and abandoned tasks
-                // for retried tasks, this does nothing (deferredCompletion is already complete)
-                bufferedTasks.forEach(t -> t.context.deferCompletion().complete());
-            }
-        }
-
-        private void handleTaskFailure(RuntimeException e, ProcessingContext<T> context, T task)
-            throws InterruptedException {
-            long currentRetryCount = context.metadata().retryCount();
-            if (maxRetryCount > currentRetryCount) {
-                logger.warn("Failed to process task<{}>, retrying ({}/{})",
-                            task, currentRetryCount, maxRetryCount, e);
-                context.retry();
-            } else {
-                logger.error("Failed to process task<{}>, retries exhausted ({})",
-                             task, currentRetryCount, e);
-            }
-        }
-
-    }
-
+    // visible for testing
     BatchingProcessor(
-        BiConsumer<String, List<BufferedTask<T>>> processor,
         long lingerMillis,
-        int capacity
+        int capacity,
+        ScheduledThreadPoolExecutor scheduledExecutor
     ) {
         this.lingerMillis = lingerMillis;
         this.capacity = capacity;
-        this.processor = processor;
+
+        if (scheduledExecutor == null) {
+            scheduledExecutor = new ScheduledThreadPoolExecutor(1, r -> {
+                Thread th = new Thread(r);
+                th.setName("Decaton" + BatchingProcessor.class.getSimpleName()
+                           + '/' + System.identityHashCode(this));
+                return th;
+            });
+        }
+        // For faster shutdown cancel all pending flush on executor shutdown.
+        // In fact for this purpose we have two options here:
+        // A. Cancel all pending flush
+        // B. Run all pending flush immediately, regardless remaining time until flush
+        // A has a downside that it forces consumer to re-process some amount of tasks.
+        // B has a downside that it may causes flush bursting and then on spike for storage/API access in
+        // downstream processor.
+        // Given that this feature is expected to be used mainly for reducing workload for storage/APIs,
+        // we think taking A is much desirable behavior in most cases.
+        scheduledExecutor.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+        executor = scheduledExecutor;
+    }
+
+    public BatchingProcessor(long lingerMillis) {
+        this(lingerMillis, Integer.MAX_VALUE, null);
+    }
+
+    public BatchingProcessor(long lingerMillis, int capacity) {
+        this(lingerMillis, capacity, null);
+    }
+
+    private void flush() {
+        if (!windowedTasks.isEmpty()) {
+            return;
+        }
+        synchronized (windowedTasks) {
+            processBatchingTasks(windowedTasks);
+        }
+    }
+
+    // visible for testing
+    Runnable flushTask() {
+        return this::flush;
+    }
+
+    private void scheduleFlush() {
+        executor.schedule(flushTask(), lingerMillis, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void process(ProcessingContext<T> context, T task) throws InterruptedException {
-        String key = context.key();
-        BufferedTaskGroup taskGroup = windowedTasks.get(key);
-        if (taskGroup != null) {
-            if (taskGroup.addBufferedTask(context, task)) {
-                logger.debug("Joined existing task group {}", taskGroup);
-                return;
-            }
+        BatchingTask<T> newTask = new BatchingTask<>(context.deferCompletion(), context, task);
+        boolean isInitialTask = windowedTasks.isEmpty();
+        windowedTasks.add(newTask);
+        if (isInitialTask) {
+            scheduleFlush();
         }
-        if (taskGroup == null) {
-            logger.debug("Creating new task group for key {}", key);
-        } else {
-            logger.debug("Cannot reuse task group {}", taskGroup);
-            taskGroup.flushNow(); // removes `taskGroup` from `windowedTasks`
+        if (windowedTasks.size() >= this.capacity) {
+            flush();
         }
-        while (true) {
-            BufferedTaskGroup freshTaskGroup = new BufferedTaskGroup(key, capacity);
-            if (freshTaskGroup.addBufferedTask(context, task)) {
-                BufferedTaskGroup replacedTaskGroup = windowedTasks.put(key, freshTaskGroup);
-                if (replacedTaskGroup != null) {
-                    logger.warn("Previous taskGroup should have been removed already, was: {}",
-                                replacedTaskGroup);
-                    replacedTaskGroup.flushNow();
-                }
-                break;
-            }
-            logger.warn("Could not add task to a fresh buffer for key {}", key);
-        }
-    }
-
-    @Override
-    public String name() {
-        return DecatonProcessor.super.name();
     }
 
     @Override
     public void close() throws Exception {
-        DecatonProcessor.super.close();
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
     }
+
+    /**
+     * *MUST* call {@link BatchingTask#completion}'s {@link DeferredCompletion#complete()} or
+     * {@link BatchingTask#context}'s {@link ProcessingContext#retry()} method.
+     */
+    abstract void processBatchingTasks(List<BatchingTask<T>> batchingTasks);
 }
