06bd37731ae79d310d25104a4492b3e09e275c66
koyapei <koyapain@gmail.com>
2023-08-08 18:39:53 +0900

    Support spring boot 3 on decaton-spring (#210)

---
+++ spring/src/integrationTestSpringBoot2/java/com/linecorp/decaton/spring/test/AutoConfigureDecatonConfigurationTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.decaton.spring.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.linecorp.decaton.spring.beans.DecatonConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = NONE)
+public class AutoConfigureDecatonConfigurationTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void test_getBean() {
+        assertThat(applicationContext.getBean(
+                DecatonConfiguration.DecatonBeanFactoryPostProcessor.class)).isNotNull();
+    }
+}
