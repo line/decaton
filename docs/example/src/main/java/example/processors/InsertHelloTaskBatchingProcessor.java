08614dd93745d59ffeb646f7c2ed1957065f3d04
Ryota Yamagishi <31466158+ryamagishi@users.noreply.github.com>
2024-07-30 18:33:50 +0900

    Add task batching doc (#150)
    * Add task batching doc
    
    * Fix task batching doc to explain how to create a class that inherits BatchingProcessor instead of direct instantiation.
    
    * Remove Implementation section from task batching doc
    
    * Upgrade task batching doc base version
    
    * Add TaskBatchingMain
    
    * Upgrade example's PROTOBUF_VERSION
    
    * Fix task batching doc to have the same code example.TaskBatchingMain
    
    * Rename task batching doc classes
    
    * Fix task batching doc's details
    
    * Add CAUTION to task batching doc

---
+++ docs/example/src/main/java/example/processors/InsertHelloTaskBatchingProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package example.processors;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.linecorp.decaton.processor.processors.BatchingProcessor;
+import com.linecorp.decaton.protocol.Sample.HelloTask;
+
+public class InsertHelloTaskBatchingProcessor extends BatchingProcessor<HelloTask> {
+    public InsertHelloTaskBatchingProcessor(long lingerMillis, int capacity) {
+        super(lingerMillis, capacity);
+    }
+
+    @Override
+    protected void processBatchingTasks(List<BatchingTask<HelloTask>> batchingTasks) {
+        List<HelloTask> helloTasks =
+                batchingTasks.stream().map(BatchingTask::task).collect(Collectors.toList());
+        helloTasks.forEach(
+                task -> System.out.println("Processing task: " + task) // (If it's real, insert tasks.)
+        );
+        batchingTasks.forEach(batchingTask -> batchingTask.completion().complete());
+    }
+}
